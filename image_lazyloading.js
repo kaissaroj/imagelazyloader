911d689492e440dd87cefdda7774a4ace7bd9dbe
Saroj Subedi <kaissaroj@gmail.com>
2020-07-23 12:51:26 +0545

    determine if any part of the element is visible in the viewport

--- image_lazyloading.js
+++ image_lazyloading.js
@@ -1,28 +1,40 @@
+(function () {
+  function isVisible(el) {
+    var top = el.offsetTop;
+    var left = el.offsetLeft;
+    var width = el.offsetWidth;
+    var height = el.offsetHeight;
 
-(function(){
-function isVisible(elem) {
-      var coords = elem.getBoundingClientRect();
-      var windowHeight = document.documentElement.clientHeight;
-      var topVisible = coords.top > 0 && coords.top < windowHeight;
-      var bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
-      return topVisible || bottomVisible;
+    while (el.offsetParent) {
+      el = el.offsetParent;
+      top += el.offsetTop;
+      left += el.offsetLeft;
     }
-    function showVisible() {
-      for (var img of document.querySelectorAll('img')) {
-        var realSrc = img.dataset.src;
-        if (!realSrc) continue;
-        if (isVisible(img)) {
-          //realSrc += '?nocache=' + Math.random(); 
-          //if we want to remove cache 
-          img.src = realSrc;
-          img.dataset.src = '';
-        }
+    return (
+      top < window.pageYOffset + window.innerHeight &&
+      left < window.pageXOffset + window.innerWidth &&
+      top + height > window.pageYOffset &&
+      left + width > window.pageXOffset
+    );
+  }
+  function showVisible() {
+    for (var img of document.querySelectorAll("img")) {
+      var realSrc = img.dataset.src;
+      if (!realSrc) continue;
+      if (isVisible(img)) {
+        //realSrc += '?nocache=' + Math.random();
+        //if we want to remove cache
+        img.src = realSrc;
+        img.dataset.src = "";
       }
-     if(Array.from(document.querySelectorAll('[data-src]')).every(function(img){
-         return img.getAttribute('data-src') === '';
-       }))   window.removeEventListener('scroll', showVisible);
     }
-    window.addEventListener('scroll', showVisible);
-    showVisible();  
+    if (
+      Array.from(document.querySelectorAll("[data-src]")).every(function (img) {
+        return img.getAttribute("data-src") === "";
+      })
+    )
+      window.removeEventListener("scroll", showVisible);
+  }
+  window.addEventListener("scroll", showVisible);
+  showVisible();
 })();
-
